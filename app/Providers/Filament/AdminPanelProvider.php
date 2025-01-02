128a29ddfced1ab8de0ae2d45a01b04b561237eb
Yuri do Monte <yspm.developer@gmail.com>
2025-01-02 10:43:09 -0300

    rename sistem

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -34,7 +34,7 @@ class AdminPanelProvider extends PanelProvider
             ])
             ->font('Nunito')
             //->topNavigation()
-            ->brandName('Laravel Financias')
+            ->brandName('Sistema Financeiro')
             ->viteTheme('resources/css/filament/admin/theme.css')
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
